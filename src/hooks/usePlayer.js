8ee46afa696656cc34d266a1f38a9b910a99f342
Arjun <59514930+arjundha@users.noreply.github.com>
2023-04-26 14:48:24 -0700

    [hooks] Super important bug fix to prevent double renders Strict React things

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -17,8 +17,8 @@ export const usePlayer = () => {
         setPlayer(prev => ({
             ...prev,
             pos: { 
-                x: (prev.pos.x += x), 
-                y: (prev.pos.y += y)
+                x: (prev.pos.x + x), 
+                y: (prev.pos.y + y)
             },
             collided,
         }));

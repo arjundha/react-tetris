acdeeecdcd50094315ad35be16387795e99f5d4e
Arjun <59514930+arjundha@users.noreply.github.com>
2023-04-26 14:49:38 -0700

    [game] Used collision checks and added gameOver functionality

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 
 // Styled Components
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
@@ -20,21 +20,36 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   // TODO: remove eslint overrides
   const movePlayer = dir => {
-    updatePlayerPos({x: dir, y: 0});
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({x: dir, y: 0});
+    }
   }
 
   // Reset all values
   const startGame = () => {
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   }
 
   const drop = () => {
-    updatePlayerPos({x: 0, y: 1, collided: false});
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({x: 0, y: 1, collided: false});
+    }
+    else {
+      if (player.pos.y < 1) {
+        console.log("Game Over!");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      // Do NOT move the tetromino --> it has collided
+      console.log("collided!");
+      updatePlayerPos({x: 0, y: 0, collided: true});
+    }
   }
 
   const dropPlayer = () => {

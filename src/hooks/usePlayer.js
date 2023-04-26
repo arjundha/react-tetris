8480573fd607ae95349806ca8ea60edcf13a7cef
Arjun <59514930+arjundha@users.noreply.github.com>
2023-04-25 17:21:00 -0700

    [components] Use custom hooks in Tetris And added comments to remember to remove eslint overrides

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { generateRandomTetromino } from '../tetrominos';
 
 export const usePlayer = () => {
+    // TODO: remove eslint override
     // eslint-disable-next-line no-unused-vars
     const [player, setPlayer] = useState({
         pos: { x: 0, y: 0 },
